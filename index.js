1f6d3bae889fe84e32683a54ea2691f965b5a79b
TanzidAnan <tanzid.anan125@gmail.com>
2025-03-12 06:10:20 +0600

    data load

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 
-const uri = `mongodb+srv://<db_username>:<db_password>@cluster0.e72gk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e72gk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,9 +31,20 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // jobs collection
+
+    const jobCollections =client.db('job-portal-1').collection('jobs')
+
+    app.get('/jobs',async(req,res ) =>{
+        const cursor =jobCollections.find();
+        const result =await cursor.toArray();
+        res.send(result)
+    })
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
