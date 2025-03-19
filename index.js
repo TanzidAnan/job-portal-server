d7e359ba85baaab1115f33d2a74c114d6d92498a
TanzidAnan <tanzid.anan125@gmail.com>
2025-03-20 02:12:04 +0600

    credentials

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin:['http://localhost:5173'],
+  credentials:true
+}));
 app.use(express.json());
 app.use(cookieParser())
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -44,11 +47,12 @@ async function run() {
       const user =req.body;
       const token =jwt.sign(user,process.env.JWT_SECRET,{expiresIn:'1r'});
       res
-      .cookie('token',token{
+      .cookie('token',token,{
         httpOnly:true,
-        secure:false
+        secure:false,
+        // http://localhost:5173/signin
       })
-      .send(token)
+      .send({success:true})
     })
 
     app.get('/jobs', async (req, res) => {
